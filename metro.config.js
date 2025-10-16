fb790daa95fdba75e79ec431c5bf45f435a67bbc
Rathees Kumar <rathees2006@gmail.com>
2025-10-16 16:58:32 +0530

    fixes in vercel deployment

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,15 @@ config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 // Add support for additional file extensions
 config.resolver.sourceExts.push('cjs');
 
+// Add Node.js polyfills for web builds
+config.resolver.alias = {
+  ...config.resolver.alias,
+  stream: 'stream-browserify',
+  crypto: 'crypto-browserify',
+  buffer: 'buffer',
+  process: 'process/browser',
+};
+
 // Configure transformer for better web support
 config.transformer.minifierConfig = {
   keep_fnames: true,
